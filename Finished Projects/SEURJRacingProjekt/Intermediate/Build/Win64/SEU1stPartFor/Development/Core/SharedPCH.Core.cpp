188c24957ca9179219966e44a52ddbdf296ba47d
TheRealHuzy <97812292+TheRealHuzy@users.noreply.github.com>
2022-05-13 11:30:35 +0200

    games

--- Finished Projects/SEURJRacingProjekt/Intermediate/Build/Win64/SEU1stPartFor/Development/Core/SharedPCH.Core.cpp
+++
@@ -1,1 +0,0 @@
-#include "X:/Unreal projects/SEU1stPartFor/Intermediate/Build/Win64/SEU1stPartFor/Development/Core/SharedPCH.Core.h"
--- Finished Projects/SEURJRacingProjekt/Intermediate/Source/SEU1stPartFor.cpp
+++
@@ -1,4 +0,0 @@
-#include "CoreTypes.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, SEU1stPartFor, "SEU1stPartFor");
